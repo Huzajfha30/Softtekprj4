e2230c729d3ba4dbef5866eb393c6738bc509c9a
Huzajfha Bayoumi <hubay22@student.sdu.dk>
2025-04-24 14:58:10 +0200

    spring boot dependencies for agv, assembly, and warehouse

---
+++ AGV/src/main/java/dk/sdu/sm4/agv/AgvApplication.java
@@ -0,0 +1,11 @@
+package dk.sdu.sm4.agv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgvApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgvApplication.class, args);
+    }
+}
