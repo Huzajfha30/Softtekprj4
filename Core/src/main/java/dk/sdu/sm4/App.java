c8a79636cfce634298f4825b891160304edfa618
hehej20 <hehej20@student.sdu.dk>
2025-04-29 11:08:36 +0200

    Forbindelse til Front-end

--- Core/src/main/java/dk/sdu/sm4/App.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.sm4;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ Core/src/main/java/dk/sdu/sm4/UI/controller/HelloController.java
@@ -0,0 +1,15 @@
+package dk.sdu.sm4.UI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hej fra Softtekprj4!";
+    }
+}
