f57fe471a6418444958e47cccf7ca7df47e9f022
hehej20 <hehej20@student.sdu.dk>
2025-05-01 10:12:16 +0200

    Update fra Nico

--- AssemblyStation/src/main/java/dk/sdu/sm4/assemblystation/App.java
+++ AssemblyStation/src/main/java/dk/sdu/sm4/assemblystation/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class App { // hej: http://localhost:8080/swagger-ui/index.html#/mqtt-controller/testMqtt
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
