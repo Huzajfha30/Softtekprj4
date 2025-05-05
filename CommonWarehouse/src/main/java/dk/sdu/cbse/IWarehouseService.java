7f0c3489f9f2fc13d4adb238765510c1cf3e3d1e
Lalor19 <lalor19@student.sdu.dk>
2025-05-05 22:07:22 +0200

    WarehouseInterface

---
+++ CommonWarehouse/src/main/java/dk/sdu/cbse/IWarehouseService.java
@@ -0,0 +1,13 @@
+package dk.sdu.cbse;
+
+import java.lang.reflect.Array;
+
+public interface IWarehouseService {
+    void pickItem(int trayId);
+    void insertItem(int trayId, java.lang.String name);
+    java.lang.String getInventory();
+    boolean pickOrderItems(int[] trayIds); // boolean siger enden: alle items var til stede i warehouse eller alle items var ikke til stede i warehouse
+    boolean stopProcess();
+    boolean restartProcess();
+    void setStockThreshold(int trayId, int threshold);
+}
