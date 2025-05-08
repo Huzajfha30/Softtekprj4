2e0acf3629c1a99966625c94547642055ea5b065
Lalor19 <lalor19@student.sdu.dk>
2025-05-08 09:39:39 +0200

    Update IWarehouseService.java

--- CommonWarehouse/src/main/java/dk/sdu/cbse/IWarehouseService.java
+++ CommonWarehouse/src/main/java/dk/sdu/cbse/IWarehouseService.java
@@ -6,7 +6,7 @@ public interface IWarehouseService {
     void pickItem(int trayId);
     void insertItem(int trayId, java.lang.String name);
     java.lang.String getInventory();
-    boolean pickOrderItems(int[] trayIds); // boolean siger enden: alle items var til stede i warehouse eller alle items var ikke til stede i warehouse
+    boolean pickOrderItems(int[] trayIds); // boolean siger enten: alle items var til stede i warehouse eller alle items var ikke til stede i warehouse
     boolean stopProcess();
     boolean restartProcess();
     void setStockThreshold(int trayId, int threshold);
