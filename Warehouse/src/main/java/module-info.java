0b1ff62524d758081a7929e02a28340ee1c8bc62
Lalor19 <lalor19@student.sdu.dk>
2025-05-15 14:32:21 +0200

    Module-info update

--- Warehouse/src/main/java/module-info.java
+++ Warehouse/src/main/java/module-info.java
@@ -2,7 +2,14 @@ module Warehouse {
     requires com.fasterxml.jackson.databind;
     requires spring.context;
     requires java.xml;
-    requires axis.jaxrpc;
     requires java.rmi;
     requires CommonWarehouse;
+    requires axis;
+    requires jaxrpc.api;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+    requires jakarta.validation;
+    requires io.swagger.v3.oas.annotations;
+    requires spring.webmvc;
 }
